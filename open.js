76da70ffb88efaf8812168a980359cd63967e949
hetshah136-hash <hetshah136@gmail.com>
2026-01-20 02:20:09 +0530

    Update open.js

--- open.js
+++ open.js
@@ -2,6 +2,5 @@ const card = document.getElementById("card");
 const overlay = document.getElementById("overlay");
 
 card.addEventListener("click", () => {
-  card.style.opacity = "0";
   overlay.classList.add("active");
 });
